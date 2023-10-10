90c24303856fc9ceb39d004337e8bc2fadc59fc5
ProgramadorOrientadoaObjeto <eduardosbarros.ed@gmail.com>
2023-10-10 13:11:30 -0300

    AULA 02

--- src/main/java/br/com/danieleleao/todolist/user/UserController.java
+++ src/main/java/br/com/danieleleao/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.danieleleao.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
-    public void create() {
 
-    }
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel UserModel) {
-         System.out.println(UserModel.name);
+    public ResponseEntity create(@RequestBody UserModel UserModel) {
+        var user = this.userRepository.findByUsername(UserModel.getUsername());
+
+        if (user != null) {
+            System.out.println("Usuário já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(UserModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
